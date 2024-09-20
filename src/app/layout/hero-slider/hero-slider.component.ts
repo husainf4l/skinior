e3aef77932baf2a0abf79684d7f50d533189eacb
Al-hussein Abdullah <al-husseinabdullah@Al-husseins-Mac-Studio.local>
2024-09-20 18:14:26 +0300

    Updated homepage with new design and tailored content for Gixat

---
+++ src/app/layout/hero-slider/hero-slider.component.ts
@@ -0,0 +1,41 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  standalone: true,
+  selector: 'app-hero-slider',
+  templateUrl: './hero-slider.component.html',
+  styleUrls: ['./hero-slider.component.css'],
+  imports: [CommonModule, RouterLink]
+})
+export class HeroSliderComponent {
+  slides: string[] = [
+    'assets/images/slide1.png',
+
+  ];
+  currentSlide = 0;
+  slideInterval: any;
+
+  ngOnInit() {
+    this.startSlideShow();
+  }
+
+  startSlideShow() {
+    this.slideInterval = setInterval(() => {
+      this.nextSlide();
+    }, 5000); // Change slide every 5 seconds
+  }
+
+  nextSlide() {
+    this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+  }
+
+  prevSlide() {
+    this.currentSlide = (this.currentSlide - 1 + this.slides.length) % this.slides.length;
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.slideInterval);
+  }
+}
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeroSliderComponent } from "../../layout/hero-slider/hero-slider.component";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [HeroSliderComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
