943189a1dc488f86d72a9b825448c5de35e1e999
husainf4l <husain.f4l@gmail.com>
2024-10-18 16:01:14 +0300

    hero

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { HeroContent } from '../../services/models/interfaces.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrl: './hero.component.css',
 })
 export class HeroComponent {
-
+  heroContent: HeroContent = {
+    title: 'اكتشفي جمالك مع سكينيور',
+    description:
+      'تميزي بأفضل منتجات العناية بالبشرة، المصممة لإبراز جمالك الطبيعي. اختبري الأناقة في أبسط تفاصيلك.',
+    buttonText: 'تسوقي الآن',
+    buttonLink: '/shop',
+    learnMoreText: 'اكتشفي المزيد',
+    learnMoreLink: '/learn-more',
+    imageUrl: './assets/images/8.jpeg',
+  };
 }
