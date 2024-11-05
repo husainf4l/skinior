2eebf5176c06445af6483aa10af6050cad4213df
alhussein <husain.f4l@gmail.com>
2024-11-05 15:10:20 +0300

    col3

--- src/app/enviroments/enviroment.ts
+++ src/app/enviroments/enviroment.ts
@@ -1,6 +1,7 @@
 export const environmenthome = {
     production: false,
     apiUrl: 'http://192.168.1.78:3000/api',
+    
 }; export const environment = {
     production: false,
     apiUrl: 'http://192.168.0.198:3000/api',
