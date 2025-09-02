46b50fa2aeaa723db95af5b92c092a320802f9a3
al-hussein abdullah <al-hussein@papayatrading.com>
2025-09-02 18:40:56 +0300

    ASDA

--- skiniorx/macos/Flutter/GeneratedPluginRegistrant.swift
+++ skiniorx/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,16 +5,24 @@
 import FlutterMacOS
 import Foundation
 
+import device_info_plus
 import firebase_auth
 import firebase_core
+import firebase_messaging
+import flutter_local_notifications
 import google_sign_in_ios
+import package_info_plus
 import path_provider_foundation
 import sign_in_with_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
 }
